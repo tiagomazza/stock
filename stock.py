6049f9016276ddf235837f75ea5804f74e4a73eb
tiagomazza <110558042+tiagomazza@users.noreply.github.com>
2024-04-03 13:47:39 +0100

    update

--- stock.py
+++ stock.py
@@ -1,38 +1,11 @@
 import streamlit as st
-import gspread
-from oauth2client.service_account import ServiceAccountCredentials
+from streamlit_gsheets import GSheetsConnection
 
-# Configurações da aplicação Streamlit
-st.title("Acesso ao Google Sheets")
-st.write("Esta aplicação acessa um Google Sheets público.")
+# Create a connection object.
+conn = st.connection("gsheets", type=GSheetsConnection)
 
-# Autenticação e acesso ao Google Sheets
-scope = ['https://spreadsheets.google.com/feeds',
-         'https://www.googleapis.com/auth/drive']
-credentials = ServiceAccountCredentials.from_json_keyfile_name('credentials.json', scope)
-client = gspread.authorize(credentials)
+df = conn.read()
 
-# Abre o Google Sheets
-sheet = client.open_by_url("https://docs.google.com/spreadsheets/d/18PgkCySwFnghk_iHsW6bpzkqjnbtsbG4y-UH_dinO_E/edit#gid=0").sheet1
-
-# Função para escrever dados
-def escrever_dados(dados):
-    sheet.append_row(dados)
-    st.success("Dados adicionados com sucesso!")
-
-# Função para apagar dados
-def apagar_dados():
-    sheet.clear()
-    st.success("Todos os dados foram apagados!")
-
-# Interface do Streamlit
-opcao = st.radio("Escolha uma opção:", ("Escrever Dados", "Apagar Dados"))
-
-if opcao == "Escrever Dados":
-    dados = st.text_input("Digite os dados separados por vírgula:")
-    if st.button("Adicionar Dados"):
-        dados = dados.split(",")
-        escrever_dados(dados)
-elif opcao == "Apagar Dados":
-    if st.button("Apagar Todos os Dados"):
-        apagar_dados()
+# Print results.
+for row in df.itertuples():
+    st.write(f"{row.name} has a :{row.pet}:")
