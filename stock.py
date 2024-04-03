e447a52193db8d21ba4185eabd46ae00b3dc6ecb
tiagomazza <110558042+tiagomazza@users.noreply.github.com>
2024-04-03 15:14:40 +0100

    s

--- stock.py
+++ stock.py
@@ -1,51 +1,11 @@
 import streamlit as st
-import pandas as pd
-from gsheet_connection import GSheetConnection
+from streamlit_gsheets import GSheetsConnection
 
-st.title("Google Sheets as a DataBase")
+# Create a connection object.
+conn = st.connection("gsheets", type=GSheetsConnection)
 
-# Function to create a sample Orders dataframe
-def create_orders_dataframe():
-    return pd.DataFrame({
-        'OrderID': [101, 102, 103, 104, 105],
-        'CustomerName': ['Alice', 'Bob', 'Charlie', 'David', 'Eve'],
-        'ProductList': ['ProductA, ProductB', 'ProductC', 'ProductA, ProductC', 'ProductB, ProductD', 'ProductD'],
-        'TotalPrice': [200, 150, 250, 300, 100],
-        'OrderDate': ['2023-08-18', '2023-08-19', '2023-08-19', '2023-08-20', '2023-08-20']
-    })
+df = conn.read()
 
-# Create the Orders dataframe
-orders = create_orders_dataframe()
-
-# Update the TotalPrice column in the orders dataframe to create updated_orders
-updated_orders = orders.copy()
-updated_orders['TotalPrice'] = updated_orders['TotalPrice'] * 100
-
-with st.expander("Data ⤵"):
-    st.write("Orders")
-    st.dataframe(orders)
-    st.write("Updated Orders")
-    st.dataframe(updated_orders)
-
-st.divider()
-st.write("CRUD Operations:")
-# Establishing a Google Sheets connection
-conn = st.connection("gsheets", type=GSheetConnection)
-
-# Taking actions based on user input
-if st.button("New Worksheet"):
-    conn.create(worksheet="Orders", data=orders)
-    st.success("Worksheet Created 🎉")
-
-if st.button("Calculate Total Orders Sum"):
-    sql = 'SELECT SUM("TotalPrice") as "TotalOrdersPrice" FROM Orders;'
-    total_orders = conn.query(sql=sql)  # default ttl=3600 seconds / 60 min
-    st.dataframe(total_orders)
-
-if st.button("Update Worksheet"):
-    conn.update(worksheet="Orders", data=updated_orders)
-    st.success("Worksheet Updated 🤓")
-
-if st.button("Clear Worksheet"):
-    conn.clear(worksheet="Orders")
-    st.success("Worksheet Cleared 🧹")
+# Print results.
+for row in df.itertuples():
+    st.write(f"{row.name} has a :{row.pet}:")
