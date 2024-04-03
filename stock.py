6c241d2b7f46f8c2a77cdc8939be9885d59c19ba
tiagomazza <110558042+tiagomazza@users.noreply.github.com>
2024-04-03 14:44:00 +0100

    update

--- stock.py
+++ stock.py
@@ -1,9 +1,12 @@
 import streamlit as st
-from streamlit_gsheets import GSheetsConnection
+from streamlit_gsheets import GSheetClient
 
 st.title("Read Google Sheet as DataFrame")
 
-conn = st.experimental_connection("gsheets", type=GSheetsConnection)
-df = conn.read(worksheet="Pag1")
+client = st.experimental_gcs_client()
+
+with client.open_by_url("https://docs.google.com/spreadsheets/d/18PgkCySwFnghk_iHsW6bpzkqjnbtsbG4y-UH_dinO_E/edit#gid=0") as sh:
+    worksheet = sh.worksheet("Pag1")
+    df = worksheet.get_as_df()
 
 st.dataframe(df)
