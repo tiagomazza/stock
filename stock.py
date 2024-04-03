398dd736de5de13d9aec4efab92fa68f74c9864d
tiagomazza <110558042+tiagomazza@users.noreply.github.com>
2024-04-03 14:27:49 +0100

    up

--- stock.py
+++ stock.py
@@ -1,20 +1,22 @@
 import streamlit as st
 from streamlit_gsheets import GSheetsConnection
 
-# Create a connection object.
-conn = st.connection("gsheets", type=GSheetsConnection)
+try:
+    # Create a connection object.
+    conn = st.connection("gsheets", type=GSheetsConnection)
 
-df = conn.read(
-    worksheet="Sheet1",
-    ttl="10m",
-    usecols=[0, 1],
-    nrows=3,
-)
+    # Lê os dados da planilha
+    df = conn.read(
+        worksheet="Sheet1",
+        ttl="10m",
+        usecols=[0, 1],
+        nrows=3,
+    )
 
-
-
-for row in df.itertuples():
-    st.write(f"{row.name} has a :{row.pet}:")
-    except Exception as e:
+    # Mostra os resultados
+    for row in df.itertuples():
+        st.write(f"{row.name} has a {row.pet}")
+except Exception as e:
     st.error(f"Erro ao ler dados do Google Sheets: {e}")
+
 
